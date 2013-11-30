e04d94d754daf2ce73f17dc2c75a2872e763f635
Marat Zaynutdinov <tsundokum@gmail.com>
2013-11-30 23:24:30 +0400

    entities template file

--- src/zakupki_gov_parser/db.clj
+++ src/zakupki_gov_parser/db.clj
@@ -5,19 +5,16 @@
               :subprotocol "sqlite"
               :subname "zakupki.sqlite"})
 
-(jdbc/db-do-commands db-spec
+(comment jdbc/db-do-commands db-spec
                      (jdbc/create-table-ddl
                       :lots
                       [:id "varchar"]))
 
 (comment jdbc/insert! db-spec
               :lots
-              {:id "my-id-is-1"})
+              {:id "my-id-is-2"})
 
-(println (doall (jdbc/query db-spec
-                            ["SELECT name FROM sqlite_master WHERE type = ?" "table"])))
-
-;(jdbc/query db-spec ["select * from lots"])
+(jdbc/query db-spec ["select * from lots"])
 
 (comment println (jdbc/create-table-ddl
                   :lots
--- src/zakupki_gov_parser/entities.clj
+++ src/zakupki_gov_parser/entities.clj
@@ -1,3 +1,18 @@
 (ns zakupki-gov-parser.entities)
 
-(def lots-entity (+ 1 1))
+;; entities have
+;; * fields with types
+;; * xml-tag or filename mask
+
+
+(def lot-entity #{:oos:ordinalNumber :oos:notificationNumber :oos:createDate :oos:regNum})
+
+(def contact-entity #{:oos:firstName})
+
+(defn list-entities []
+  (let [internal-symbols (-> *ns*
+                              ns-interns
+                              keys)]
+    (filter #(.endsWith (name %) "-entity") internal-symbols)))
+
+(println (list-entities))
